9226d51b6b6fe86aecf57813e23279d12a796c9c
daveshrestha-intel <dave.shrestha@intel.com>
2023-12-19 08:40:25 -0800

    Created RHEL example with TDX

---
+++ examples/azure-windows-tdx-vm-existing-network/main.tf
@@ -0,0 +1,15 @@
+###  required, resource group, subnet_name, virtual_network_name
+// TODO - update required section with reference to terraform.tfvars file for the 3 fields needed. 
+
+module "azure-windows-vm" {
+  source                       = "intel/azure-windows-vm/intel"
+  admin_password               = var.admin_password
+  azurerm_resource_group_name  = "shreejan_test_mssql"
+  azurerm_subnet_name          = "dbx-public"
+  azurerm_virtual_network_name = "mssql_vnet_test"
+  tags = {
+    owner    = "owner_josh",
+    duration = "5"
+  }
+}
+
